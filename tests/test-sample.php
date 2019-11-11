1ab5a56f0fdd23588a5db0f02a9eab61762f0f17
Kimiya Kitani <kimipooh@gmail.com>
2019-11-11 13:49:49 +0900

    fixed tested environments

--- tests/test-sample.php
+++ tests/test-sample.php
@@ -1,10 +1,20 @@
 <?php
+/**
+ * Class SampleTest
+ *
+ * @package List_View_Google_Calendar
+ */
 
+/**
+ * Sample test case.
+ */
 class SampleTest extends WP_UnitTestCase {
 
-	function test_sample() {
-		// replace this with some actual testing code
+	/**
+	 * A single example test.
+	 */
+	public function test_sample() {
+		// Replace this with some actual testing code.
 		$this->assertTrue( true );
 	}
 }
-
